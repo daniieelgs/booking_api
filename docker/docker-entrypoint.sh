147d26f6f539c14fb0e248eedaa9f87bf8db634d
Daniel Garcia <daniieelgs@gmail.com>
2024-10-18 18:24:09 +0200

    refactor docker prod

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Exportar variables necesarias para envsubst
-export PORT SSL_CERT_FILE SSL_KEY_FILE
+export PORT SSL_CERT_FILE SSL_KEY_FILE SERVER_NAME
 
 # Generar el archivo de configuración de Apache a partir de la plantilla
-envsubst '$PORT $SSL_CERT_FILE $SSL_KEY_FILE' < /app/apache-flask.conf.template > /etc/apache2/sites-available/000-default.conf
+envsubst '$PORT,$SSL_CERT_FILE,$SSL_KEY_FILE,$SERVER_NAME' < /app/apache-flask.conf.template > /etc/apache2/sites-available/000-default.conf
 
 # Habilitar módulos necesarios de Apache
 a2enmod ssl
